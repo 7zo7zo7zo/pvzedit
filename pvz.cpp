961c767aadd323ac2d3d241ed850567890bf866c
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-08-29 11:39:17 -0700

    Update pvz.cpp

--- pvz.cpp
+++ pvz.cpp
@@ -7,42 +7,42 @@ using namespace std;
 HANDLE hpro;
 
 void WriteMemory(DWORD val, DWORD size, DWORD base, int argnum, ...) {
-    if (!hpro)
-        return;
-    if (size > 4)
-        size = 4;
-    if (argnum == 0) {
-        if(WriteProcessMemory(hpro, LPVOID(base), &val, size, 0)) {
-	    	//MessageBox(NULL, "WriteProcessMemory worked.", "Success", MB_OK + MB_ICONINFORMATION);
+	if (!hpro)
+	    return;
+	if (size > 4)
+	    size = 4;
+	if (argnum == 0) {
+		if(WriteProcessMemory(hpro, LPVOID(base), &val, size, 0)) {
+			//MessageBox(NULL, "WriteProcessMemory worked.", "Success", MB_OK + MB_ICONINFORMATION);
 		cout << "Success!" << endl;
 		}
 		else {
 			MessageBox(NULL, "Error cannot WriteProcessMemory!", "Error", MB_OK + MB_ICONERROR);
 		}
-    }
-    else {
-        DWORD temp;
-        ReadProcessMemory(hpro, LPCVOID(base), &temp, 4, 0);
-        va_list args;
-        va_start(args, argnum);
-        for (int i = 1; i < argnum; ++i) {
-            ReadProcessMemory(hpro, LPCVOID(temp + va_arg(args, int)), &temp, 4, 0);
-        }
-        if(WriteProcessMemory(hpro, LPVOID(temp + va_arg(args, int)), &val, size, 0)) {
-		//MessageBox(NULL, "WriteProcessMemory worked.", "Success", MB_OK + MB_ICONINFORMATION);
-		cout << "Success!" << endl;
+	}
+	else {
+		DWORD temp;
+		ReadProcessMemory(hpro, LPCVOID(base), &temp, 4, 0);
+		va_list args;
+		va_start(args, argnum);
+		for (int i = 1; i < argnum; ++i) {
+			ReadProcessMemory(hpro, LPCVOID(temp + va_arg(args, int)), &temp, 4, 0);
+		}
+		if(WriteProcessMemory(hpro, LPVOID(temp + va_arg(args, int)), &val, size, 0)) {
+			//MessageBox(NULL, "WriteProcessMemory worked.", "Success", MB_OK + MB_ICONINFORMATION);
+			cout << "Success!" << endl;
 		}
 		else {
 			MessageBox(NULL, "Error cannot WriteProcessMemory!", "Error", MB_OK + MB_ICONERROR);
 		}
-    	va_end(args);
+		va_end(args);
     }
 }
 
 void mainLoop() {
 	for(int i = 0; i < 10; i++) {
-    		cout << "What would you like to modify? ";
-    		string input;
+			cout << "What would you like to modify? ";
+			string input;
 		cin >> input;
 		
 		if(input == "exit") {
@@ -128,16 +128,16 @@ void mainLoop() {
 
 int main() {
 	HWND hWnd = FindWindow(0, "Plants Vs. Zombies");
-  	if(hWnd == 0) {
-    		MessageBox(0, "Error cannot find window.", "Error", MB_OK|MB_ICONERROR);
-  	}
+	if(hWnd == 0) {
+		MessageBox(0, "Error cannot find window.", "Error", MB_OK|MB_ICONERROR);
+	}
 	else {
-    		DWORD proccess_ID;
-    		GetWindowThreadProcessId(hWnd, &proccess_ID);
-    		hpro = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proccess_ID);
-    		if(!hpro) {
-      			MessageBox(0, "Could not open the process!", "Error!", MB_OK|MB_ICONERROR);
-    		}
+			DWORD proccess_ID;
+			GetWindowThreadProcessId(hWnd, &proccess_ID);
+			hpro = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proccess_ID);
+			if(!hpro) {
+	  			MessageBox(0, "Could not open the process!", "Error!", MB_OK|MB_ICONERROR);
+			}
 		else {
 			mainLoop();
 		}
